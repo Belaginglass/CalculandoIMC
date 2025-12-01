d0bec7e19182083a8c45559ebe65e2e04432049b
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-12-01 08:07:33 -0300

    corrigindo

--- src/main/java/AnalisaIMC.java
+++ src/main/java/AnalisaIMC.java
@@ -1,4 +1,4 @@
-public class CalcularIMC {
+public class AnalisaIMC {
 
     public static float calculaIMC(float peso, float altura) {
         return peso / (altura * altura);
@@ -33,4 +33,22 @@ public class CalcularIMC {
     }
 
 
+    public static String classificarIMC(PessoaInterface p) {
+        float imc = calculaIMC(p.getPeso(), p.getAltura());
+        if (p.getSexo().equalsIgnoreCase("homem")) {
+            if (imc < 20.7) return "Abaixo do peso";
+            else if (imc < 26.4) return "Peso normal";
+            else if (imc < 27.8) return "Marginalmente Acima do Peso Ideal";
+            else if (imc < 31.1) return "Acima do Peso Ideal";
+            else return "Obeso";
+        } else if (p.getSexo().equalsIgnoreCase("mulher")) {
+            if (imc < 19.1) return "Abaixo do peso";
+            else if (imc < 25.8) return "Peso normal";
+            else if (imc < 27.3) return "Marginalmente Acima do Peso Ideal";
+            else if (imc < 32.3) return "Acima do Peso Ideal";
+            else return "Obeso";
+        } else {
+            return "Sexo inválido";
+        }
+    }
 }
